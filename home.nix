8835a3e5d834d2fb0276f0b924c3efde94d3f95d
Keranod <konrad.konkel@wp.pl>
2025-01-29 19:10:08 +0000

    installed postgres on machine and pgadmin4-desktop for user

--- home.nix
+++ home.nix
@@ -28,6 +28,7 @@
     microsoft-edge
     nixd # nix language server
     nixfmt-rfc-style
+    pgadmin4-desktopmode
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
