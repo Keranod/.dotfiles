13833bed15cc3e00265a081e709ceaf3b0adce17
Keranod <konrad.konkel@wp.pl>
2025-01-29 19:10:08 +0000

    installed postgres on machine and pgadmin4-desktop for user

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
     nodejs_23
+    postgresql_17
     home-manager
 #  wget
   ];
