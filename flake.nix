39a0431b4617b33fc07c41a54ae0eabd7a5db924
Keranod <konrad.konkel@wp.pl>
2025-01-29 19:38:19 +0000

    removed unused inport

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
 
   inputs = {
     # Tells flake where to look for packages
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-24.11";
+      };
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
