7c2311747863fbab925b2a744cd480c66b01181a
Keranod <70093810+Keranod@users.noreply.github.com>
2025-01-30 18:45:37 +0000

    Update configuration.nix

--- hosts/TufNix/configuration.nix
+++ hosts/TufNix/configuration.nix
@@ -51,9 +51,9 @@ in
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
+  # Enable the SDDM display manager
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
