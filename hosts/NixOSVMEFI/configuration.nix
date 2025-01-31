23e18ca823990813aaf6b37a76620702590eba5c
Keranod <70093810+Keranod@users.noreply.github.com>
2025-01-31 09:02:06 +0000

    Update configuration.nix

--- hosts/NixOSVMEFI/configuration.nix
+++ hosts/NixOSVMEFI/configuration.nix
@@ -23,7 +23,7 @@ in
   # Networking
   networking.hostName = "NixOSVMEFI"; # Define your hostname.
   networking.networkmanager.enable = true;
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
