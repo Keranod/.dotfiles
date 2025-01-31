dfeaab372c3329577f824b04bee0e73b919e3d46
Keranod <konrad.konkel@wp.pl>
2025-01-31 11:08:17 +0000

    testing first boot script

--- firstBootScript.nix
+++ firstBootScript.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 
 let
-  # Path to your script
   firstBootScript = "/home/keranod/.dotfiles/firstBootScript.sh";
 in
 {
@@ -9,15 +8,13 @@ in
     description = "firstBootScript";
     after = [ "network.target" ];
 
-    # Run as root user to allow sudo privileges in the script
-    user = "root";
-    
-    # Execute the script
     serviceConfig.ExecStart = ''
-      sudo ${pkgs.bash}/bin/bash ${firstBootScript}
+      if [ ! -x ${firstBootScript} ]; then
+        chmod +x ${firstBootScript}
+      fi
+      ${pkgs.bash}/bin/bash ${firstBootScript}
     '';
 
-    # Make the service a one-shot service
     wantedBy = [ "multi-user.target" ];
   };
 }
