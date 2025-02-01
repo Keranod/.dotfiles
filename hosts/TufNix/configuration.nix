d92f4ccb25d4a9824f918e8f7b317513be2ed6db
keranod <konrad.konkel@wp.pl>
2025-02-01 10:23:18 +0000

    cleaning up GNOME, configuring GNOME, trying gpu settings

--- hosts/TufNix/configuration.nix
+++ hosts/TufNix/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 let
   postgresVersion = "17";  # Define PostgreSQL version once
@@ -47,10 +47,31 @@ in
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable GNOME 
+  # GNOME 
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gnome-weather
+    gnome-maps
+    totem
+    gedit
+    cheese
+    gnome-music
+    epiphany
+    geary
+    gnome-characters 
+    tali
+    iagno
+    hitori
+    atomix
+    yelp
+    gnome-initial-setup
+    #gnome-contacts
+  ]);
+  programs.dconf.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -72,75 +93,20 @@ in
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  hardware.graphics = {
-  enable = true;
-  extraPackages = with pkgs; [
-    libvdpau
-    vaapiVdpau
-    libva
-    vulkan-loader
-    vulkan-validation-layers
-    nvidia-vaapi-driver  # For VAAPI support on Nvidia
-  ];
-};
-
-services.xserver.videoDrivers = [ "nvidia" ];
-
-hardware.nvidia = {
-  modesetting.enable = true;
-  powerManagement.enable = false;
-  powerManagement.finegrained = false;
-  open = false;  # Keep it false for stability
-  nvidiaSettings = true;
-  
-  prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
-    intelBusId = "PCI:00:02:0";
-    nvidiaBusId = "PCI:01:00:0";
-  };
-
-  package = config.boot.kernelPackages.nvidiaPackages.stable;
-};
-
-  
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  };  
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile. 
+  # To search, go https://search.nixos.org/packages?channel=24.11&
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     git
     nodejs_23
     home-manager
-    # cudatoolkit
-   #  wget
+    gnome.gnome-tweaks
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
@@ -151,13 +117,8 @@ hardware.nvidia = {
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  # https://mynixos.com/
+  system.stateVersion = "24.11";
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -166,6 +127,8 @@ hardware.nvidia = {
     enable = true;
     package = postgresPackage;  # Install & enable same version 
     enableTCPIP = true;
+    # Authentication only to host, cannot make local work with scram
+    # psql -U <username> -h 127.0.0.1
     authentication = ''
       #type database  DBuser  address        auth-method
       #local all       all                    peer
