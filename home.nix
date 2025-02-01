d40c1e3d98d2e808adcb70d980c150bde8a248e5
keranod <konrad.konkel@wp.pl>
2025-02-01 08:38:15 +0000

    YT music

--- home.nix
+++ home.nix
@@ -36,6 +36,7 @@ in
     pgadminPackage
     thunderbird
     google-chrome
+    youtube-music
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
--- hosts/TufNix/configuration.nix
+++ hosts/TufNix/configuration.nix
@@ -1,8 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 let
   postgresVersion = "17";  # Define PostgreSQL version once
@@ -12,26 +8,24 @@ in
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      #../../firstBootScript.nix
+      ./gpu.nix
     ];
 
+  # Default settings for EFI
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   fileSystems."/boot" = {
     fsType = "vfat";
   };
-  boot.kernelParams = [ "nouveau.modeset=0" "i915.modeset=0" "nvidia.NVreg_EnableGpuFirmware=0" ];
 
   # Networking
-  networking.hostName = "TufNix"; # Define your hostname.
+  networking.hostName = "TufNix"; 
   networking.networkmanager.enable = true;
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Set your time zone.
   time.timeZone = "Europe/London";
 
   # Select internationalisation properties.
