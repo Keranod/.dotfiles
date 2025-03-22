9ce6473db0a7b9d38ce87e462fa74cf00d4460d2
Keranod <70093810+Keranod@users.noreply.github.com>
2025-03-22 08:20:47 +0000

    Update configuration.nix
    fail2ban ssh and nginx

--- hosts/MainServer/configuration.nix
+++ hosts/MainServer/configuration.nix
@@ -129,34 +129,38 @@ boot.loader.grub.useOSProber = true;
 
   # Fail2ban global setup
   services.fail2ban = {
+  enable = true;
+  maxretry = 5;
+  ignoreIP = ["84.39.117.57"];
+  bantime = "24h";
+  bantime-increment = {
     enable = true;
-    package = fail2ban;
-    # Ban IP after 5 failures
-    maxretry = 5;
-    ignoreIP = [
-      "84.39.117.57" # whitelist a specific IP
-    ];
-    bantime = "24h"; # Ban IPs for one day on the first ban
-    bantime-increment = {
-      enable = true; # Enable increment of bantime after each violation
-      formula = "ban.Time * math.exp(float(ban.Count+1)*1.5)/math.exp(1*1.5)";
-      multipliers = "1 2 4 8 16 32 64";
-      maxtime = "168h"; # Do not ban for more than 1 week
-      overalljails = true; # Calculate the bantime based on all the violations
+    formula = "ban.Time * math.exp(float(ban.Count+1)*1.5)/math.exp(1*1.5)";
+    multipliers = "1 2 4 8 16 32 64";
+    maxtime = "168h";
+    overalljails = true;
+  };
+  jails = {
+    sshd.settings = {
+      enabled = true;
+      filter = "sshd";
+      action = "iptables-multiport[name=SSH, port=ssh]";
+      logpath = "/var/log/auth.log";
+      backend = "systemd";
+      maxretry = 3;
+      findtime = 600;
+      bantime = "24h";
     };
-    jails = {
-      apache-nohome-iptables.settings = {
-        # Block an IP address if it accesses a non-existent
-        # home directory more than 5 times in 10 minutes,
-        # since that indicates that it's scanning.
-        filter = "apache-nohome";
-        action = ''iptables-multiport[name=HTTP, port="http,https"]'';
-        logpath = "/var/log/httpd/error_log*";
-        backend = "auto";
-        findtime = 600;
-        bantime  = 600;
-        maxretry = 5;
-      };
+    nginx-badbots.settings = {
+      enabled = true;
+      filter = "nginx-badbots";
+      action = "iptables-multiport[name=HTTP, port="http,https"]";
+      logpath = "/var/log/nginx/access.log";
+      maxretry = 5;
+      findtime = 600;
+      bantime = "24h";
     };
   };
+};
+
 }
