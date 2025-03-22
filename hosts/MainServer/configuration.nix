8469071bd6135c4a0253f3537be1943e618422e5
keranod <konrad.konkel@wp.pl>
2025-03-22 19:52:56 +0000

    ngnix config

--- hosts/MainServer/configuration.nix
+++ hosts/MainServer/configuration.nix
@@ -149,6 +149,11 @@ boot.loader.grub.useOSProber = true;
         root = "/home/keranod/programming/WatchesWithMark/WatchesWithMark-frontend/dist";
         index = "index.html";
         tryFiles = "$uri $uri/ /index.html";
+
+        extraConfig = ''
+          add_header X-Frame-Options "SAMEORIGIN" always;
+          add_header X-Content-Type-Options "nosniff" always;
+        '';
       };
 
       # Cache static assets efficiently
@@ -190,9 +195,6 @@ boot.loader.grub.useOSProber = true;
         gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript image/svg+xml;
         gzip_proxied any;
         gzip_min_length 256;
-
-        add_header X-Frame-Options "SAMEORIGIN" always;
-        add_header X-Content-Type-Options "nosniff" always;
       '';
     };
   };
