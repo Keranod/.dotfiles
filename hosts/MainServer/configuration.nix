38863cc2782ca3acf3ac6bff7475c99e52734250
Keranod <70093810+Keranod@users.noreply.github.com>
2025-03-22 08:16:36 +0000

    Update configuration.nix
    fail2ban setup in MainServer

--- hosts/MainServer/configuration.nix
+++ hosts/MainServer/configuration.nix
@@ -85,7 +85,6 @@ boot.loader.grub.useOSProber = true;
     nginx
     goaccess
     certbot
-    fail2ban
     unzip
   ];
 
@@ -126,5 +125,38 @@ boot.loader.grub.useOSProber = true;
       #local all       all                    peer
       host  all       all     127.0.0.1/32   scram-sha-256
     '';
+  };
+
+  # Fail2ban global setup
+  services.fail2ban = {
+    enable = true;
+    package = fail2ban;
+    # Ban IP after 5 failures
+    maxretry = 5;
+    ignoreIP = [
+      "84.39.117.57" # whitelist a specific IP
+    ];
+    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime-increment = {
+      enable = true; # Enable increment of bantime after each violation
+      formula = "ban.Time * math.exp(float(ban.Count+1)*1.5)/math.exp(1*1.5)";
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h"; # Do not ban for more than 1 week
+      overalljails = true; # Calculate the bantime based on all the violations
+    };
+    jails = {
+      apache-nohome-iptables.settings = {
+        # Block an IP address if it accesses a non-existent
+        # home directory more than 5 times in 10 minutes,
+        # since that indicates that it's scanning.
+        filter = "apache-nohome";
+        action = ''iptables-multiport[name=HTTP, port="http,https"]'';
+        logpath = "/var/log/httpd/error_log*";
+        backend = "auto";
+        findtime = 600;
+        bantime  = 600;
+        maxretry = 5;
+      };
+    };
   };
 }
