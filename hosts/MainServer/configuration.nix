9fa736e036da3ac08581307fa351641d0c50aeab
keranod <konrad.konkel@wp.pl>
2025-03-23 20:37:31 +0000

    fixing strapi service

--- hosts/MainServer/configuration.nix
+++ hosts/MainServer/configuration.nix
@@ -229,7 +229,7 @@ boot.loader.grub.useOSProber = true;
 
     serviceConfig = {
       WorkingDirectory = "/var/www/WatchesWithMark/WatchesWithMark-backend";
-      ExecStart = "/run/current-system/sw/bin/npm start";
+      ExecStart = "/run/wrappers/bin/bash -c 'PATH=/run/current-system/sw/bin:$PATH /run/current-system/sw/bin/npm start'";
       Restart = "always";
       User = "keranod";
       Group = "users";
