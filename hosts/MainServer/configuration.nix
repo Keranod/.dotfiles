d366ff9a6258f230327ba8030a0d0da70e005ec9
Konrad Konkel <konrad.konkel@wp.pl>
2025-03-31 15:59:15 +0100

    trying to fix uploads

--- hosts/MainServer/configuration.nix
+++ hosts/MainServer/configuration.nix
@@ -202,6 +202,17 @@ boot.loader.grub.useOSProber = true;
         '';
       };
 
+      locations."^~ /uploads/" = {
+  extraConfig = ''
+    proxy_pass http://localhost:1337;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+  '';
+};
+
+
       # Rate limit API requests
       locations."~* ^/(api|uploads)/" = {
         extraConfig = ''
