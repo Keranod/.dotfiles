24edf0fd9c7bd860123de43681e3955c45df0bd9
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-07 16:29:12 +0100

    samba

--- hosts/VMNixOSWork/configuration.nix
+++ hosts/VMNixOSWork/configuration.nix
@@ -127,67 +127,67 @@ in
   services.samba = {
     enable = true;
     package = bindPkgs.bind;
-    extraConfig = ''
-      [global]
-        workgroup = PSFRANKSNET
-        security = user
-        server min protocol = CORE
-        server max protocol = NT1
-        ntlm auth = yes
+    # extraConfig = ''
+    #   [global]
+    #     workgroup = PSFRANKSNET
+    #     security = user
+    #     server min protocol = CORE
+    #     server max protocol = NT1
+    #     ntlm auth = yes
 
-        passdb backend = tdbsam
+    #     passdb backend = tdbsam
 
-        printing = cups
-        printcap name = cups
-        load printers = yes
-        cups options = raw
+    #     printing = cups
+    #     printcap name = cups
+    #     load printers = yes
+    #     cups options = raw
 
-        server string = Oracle Linux VM
-        netbios name = ENDOR
+    #     server string = Oracle Linux VM
+    #     netbios name = ENDOR
 
-        acl group control = yes
-        add user script = sudo /usr/sbin/useradd -d /home/%u -s /bin/bash %u
-        add machine script = sudo /usr/sbin/useradd -g machines -c "Samba Client" -d /dev/null -s /bin/false -M %u
-        add group script = sudo /usr/sbin/groupadd %g
-        admin users = iand
-        allow nt4 crypto = yes
-        delete user script = /usr/sbin/userdel %u
-        delete group script = /usr/sbin/groupdel %g
-        dns proxy = no
-        domain logons = yes
-        domain master = yes
-        idmap config * : range = 10000 - 10999
-        log level = 1
-        logon drive = P:
-        logon home = \\GALLIFREY\%U
-        logon path =
-        max log size = 50
-        socket options = TCP_NODELAY
-        time server = yes
-        wins support = true
+    #     acl group control = yes
+    #     add user script = sudo /usr/sbin/useradd -d /home/%u -s /bin/bash %u
+    #     add machine script = sudo /usr/sbin/useradd -g machines -c "Samba Client" -d /dev/null -s /bin/false -M %u
+    #     add group script = sudo /usr/sbin/groupadd %g
+    #     admin users = iand
+    #     allow nt4 crypto = yes
+    #     delete user script = /usr/sbin/userdel %u
+    #     delete group script = /usr/sbin/groupdel %g
+    #     dns proxy = no
+    #     domain logons = yes
+    #     domain master = yes
+    #     idmap config * : range = 10000 - 10999
+    #     log level = 1
+    #     logon drive = P:
+    #     logon home = \\GALLIFREY\%U
+    #     logon path =
+    #     max log size = 50
+    #     socket options = TCP_NODELAY
+    #     time server = yes
+    #     wins support = true
 
-      [homes]
-        comment = Home Directories
-        valid users = %S, %D%w%S
-        browseable = No
-        read only = No
-        inherit acls = Yes
+    #   [homes]
+    #     comment = Home Directories
+    #     valid users = %S, %D%w%S
+    #     browseable = No
+    #     read only = No
+    #     inherit acls = Yes
 
-      [printers]
-        comment = All Printers
-        path = /var/tmp
-        printable = Yes
-        create mask = 0600
-        browseable = No
+    #   [printers]
+    #     comment = All Printers
+    #     path = /var/tmp
+    #     printable = Yes
+    #     create mask = 0600
+    #     browseable = No
 
-      [print$]
-        comment = Printer Drivers
-        path = /var/lib/samba/drivers
-        write list = @printadmin root
-        force group = @printadmin
-        create mask = 0664
-        directory mask = 0775
-    '';
+    #   [print$]
+    #     comment = Printer Drivers
+    #     path = /var/lib/samba/drivers
+    #     write list = @printadmin root
+    #     force group = @printadmin
+    #     create mask = 0664
+    #     directory mask = 0775
+    # '';
   };
 
   # Bind
