3a4d827c57cf2e17a219094ba91495bcc97929a2
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-07 10:38:04 +0100

    creating vm for work

---
+++ hosts/VMNixOSWork/configuration.nix
@@ -0,0 +1,156 @@
+{ pkgs, ... }:
+
+# let
+#   postgresVersion = "17";  # Define PostgreSQL version once
+#   postgresPackage = pkgs."postgresql_${postgresVersion}";
+# in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Default settings for EFI
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  fileSystems."/boot" = {
+    fsType = "vfat";
+  };
+
+  # Networking
+  networking.hostName = "VMNixOSWork";
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # GNOME
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+    gnome-weather
+    gnome-maps
+    # nautilus # File manager
+    totem
+    gedit
+    cheese
+    gnome-music
+    # epiphany # needed for online accounts
+    # geary
+    gnome-characters
+    tali
+    iagno
+    hitori
+    atomix
+    yelp
+    gnome-initial-setup
+    #gnome-contacts
+  ]);
+  programs.dconf.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # For vscode extensions
+  programs.nix-ld.enable = true;
+
+  # List packages installed in system profile.
+  # To search, go https://search.nixos.org/packages?channel=24.11&
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    nodejs_22
+    home-manager
+    gnome.gnome-tweaks
+    gnome-online-accounts
+    #wireguard-tools
+    #wireguard-ui
+    # gnome-notes
+    # gnomeExtensions.brightness-control-using-ddcutil
+  ];
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ 5173 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # https://mynixos.com/
+  system.stateVersion = "24.11";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Postgres Global setup
+  # services.postgresql = {
+  #   enable = true;
+  #   package = postgresPackage;  # Install & enable same version
+  #   enableTCPIP = true;
+  #   # Authentication only to host, cannot make local work with scram
+  #   # psql -U <username> -h 127.0.0.1
+  #   authentication = ''
+  #     #type database  DBuser  address        auth-method
+  #     #local all       all                    peer
+  #     host  all       all     127.0.0.1/32   scram-sha-256
+  #   '';
+  # };
+
+  # Not working/not sorted yet
+  # Always mount second hard drive
+  # lsblk -> get /dev/<diskname>
+  # sudo blkid /dev/<diskname> -> get uuid of the disk
+  # fileSystems."/mnt/data" = {
+  #   device = "/dev/disk/by-uuid/b298f8d8-1745-4581-ad9e-a58023d83f61";
+  #   fsType = "ext4";
+  #   options = [ "defaults" "nofail" ];
+  # };
+}
---
+++ hosts/VMNixOSWork/home.nix
@@ -0,0 +1,128 @@
+{ pkgs, ... }:
+
+let
+  username = "keranod";
+  pgadminVersion = "4";
+  pgadminPackage = pkgs."pgadmin${pgadminVersion}-desktopmode";
+in
+{
+  # Infomration for home-manager which path to manage
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  #For c# in vscode extension
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-sdk-6.0.428"
+  ];
+
+  # List packages installed in user profile.
+  # To search, go https://search.nixos.org/packages?channel=24.11&
+  home.packages = with pkgs; [
+    (pkgs.buildFHSUserEnv {
+      name = "vscode-fhs";
+      targetPkgs = pkgs: [
+        pkgs.vscode
+        pkgs.icu
+      ];
+      runScript = "${pkgs.vscode}/bin/code";
+    })
+    nixd # nix language server
+    nixfmt-rfc-style
+    #pgadminPackage
+    #thunderbird
+    #google-chrome
+    vlc
+    #prismlauncher
+    dotnet-sdk_9
+    #dotnet-sdk_8
+    godot_4-mono
+    vscode
+    #discord
+  ];
+
+  nix.nixPath = [ "nixpkgs=${pkgs.path}" ];
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/keranod/etc/profile.d/hm-session-vars.sh
+  #
+
+  # TODO
+  # GNOME
+  # Run `dconf watch /` and edit settings that you want to change and apply them below
+  # notes taht sync with keep
+  # dconf watch /
+  dconf.settings = {
+    # Does not work
+    # "org/gnome/shell" = {
+    #   last-selected-power-profile = "balanced";
+    # };
+    # "org/gnome/shell" = {
+    #   disable-user-extensions = false;
+    #   disabled-extensions = [];
+    #   enabled-extensions = ["display-brightness-ddcutil@themightydeity.github.com"];
+    # };
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      natural-scroll = false;
+    };
+    "org/gnome/mutter" = {
+      edge-tiling = true;
+      dynamic-workspaces = true;
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      power-button-action = "interactive";
+      sleep-inactive-ac-type = "nothing";
+      # Does not work
+      # sleep-inactive-battery-timeout = "3600";
+    };
+  };
+
+  # User services
+  # systemd.user.services.pgadmin4 = {
+  #   Unit = {
+  #     Description = "Pgadmin web interface";
+  #     After = [ "default.target" "postgres.service" ];
+  #   };
+
+  #   Service = {
+  #     ExecStart = "${pgadminPackage}/bin/pgadmin4";
+  #     Restart = "always";
+  #     WorkingDirectory = "%h";
+  #   };
+
+  #   Install = {
+  #     WantedBy = [ "default.target" ];
+  #   };
+  # };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
