119fccda63f786c61d16e8922e73226fa3be22aa
keranod <konrad.konkel@wp.pl>
2025-04-07 16:27:24 +0100

    syncing finally

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     # Tells flake where to look for packages
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-24.11";
-      };
+    };
     nixpkgs-unstable = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
@@ -22,66 +22,88 @@
   };
 
   # Importing self ans nixpkgs
-  outputs = { nixpkgs, nixpkgs-unstable, home-manager, bindPkgs, sambaPkgs, ...}:
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-unstable,
+      home-manager,
+      ...
+    }:
     # Assagning nixpkgs.lib in the scope followed after brackets after in to variable lib
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = import nixpkgs {inherit system;};
-      bindPkgs = import bindPkgs {inherit system;};
-      sambaPkgs = import sambaPkgs {inherit system;};
-    in {
-    # Can specify multiple configurations
-    nixosConfigurations = {
-      # Any name to create system configuration, like assiging configuration object to a variable
-      TufNix = lib.nixosSystem {
-        # Architecture
-        inherit system;
-        # List/Array of modules
-        modules = [ ./hosts/TufNix/configuration.nix ./users.nix ];
-      };
-      NixOSVMEFI = lib.nixosSystem {
-        # Architecture
-        inherit system;
-        # List/Array of modules
-        modules = [ ./hosts/NixOSVMEFI/configuration.nix ./users.nix ];
-      };
-      MainServer = lib.nixosSystem {
-        # Architecture
-        inherit system;
-        # List/Array of modules
-        modules = [ ./hosts/MainServer/configuration.nix ./users.nix ];
-      };
-      VMNixOSWork = lib.nixosSystem {
-        # Architecture
-        inherit system;
-        specialArgs = { inherit bindPkgs sambaPkgs; };
-        # List/Array of modules
-        modules = [ ./hosts/VMNixOSWork/configuration.nix ./users.nix ];
-      };
-    };
-    homeConfigurations = {
-      "keranod@TufNix" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = {
-          pkgsUnstable = nixpkgs-unstable.legacyPackages.${system};
+      pkgs = import nixpkgs { inherit system; };
+      bindPkgs = import bindPkgs { inherit system; };
+      sambaPkgs = import sambaPkgs { inherit system; };
+    in
+    {
+      # Can specify multiple configurations
+      nixosConfigurations = {
+        # Any name to create system configuration, like assiging configuration object to a variable
+        TufNix = lib.nixosSystem {
+          # Architecture
+          inherit system;
+          # List/Array of modules
+          modules = [
+            ./hosts/TufNix/configuration.nix
+            ./users.nix
+          ];
         };
-        modules = [
-          ./common.nix ./hosts/TufNix/home.nix
-        ];
+        NixOSVMEFI = lib.nixosSystem {
+          # Architecture
+          inherit system;
+          # List/Array of modules
+          modules = [
+            ./hosts/NixOSVMEFI/configuration.nix
+            ./users.nix
+          ];
+        };
+        MainServer = lib.nixosSystem {
+          # Architecture
+          inherit system;
+          # List/Array of modules
+          modules = [
+            ./hosts/MainServer/configuration.nix
+            ./users.nix
+          ];
+        };
+        VMNixOSWork = lib.nixosSystem {
+          # Architecture
+          inherit system;
+          specialArgs = { inherit bindPkgs sambaPkgs; };
+          # List/Array of modules
+          modules = [
+            ./hosts/VMNixOSWork/configuration.nix
+            ./users.nix
+          ];
+        };
       };
-       "keranod@MainServer" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./common.nix ./hosts/MainServer/home.nix
-        ];
-      };
-      "keranod@VMNixOSWork" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./common.nix ./hosts/VMNixOSWork/home.nix
-        ];
+      homeConfigurations = {
+        "keranod@TufNix" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          extraSpecialArgs = {
+            pkgsUnstable = nixpkgs-unstable.legacyPackages.${system};
+          };
+          modules = [
+            ./common.nix
+            ./hosts/TufNix/home.nix
+          ];
+        };
+        "keranod@MainServer" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./common.nix
+            ./hosts/MainServer/home.nix
+          ];
+        };
+        "keranod@VMNixOSWork" = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./common.nix
+            ./hosts/VMNixOSWork/home.nix
+          ];
+        };
       };
     };
-  };
 }
