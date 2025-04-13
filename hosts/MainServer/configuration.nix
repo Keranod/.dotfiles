2ce89b7485a47acf295299e86617ffe52e9ca70d
keranod <konrad.konkel@wp.pl>
2025-04-13 15:39:04 +0100

    trying to fix issue with sending big files

--- hosts/MainServer/configuration.nix
+++ hosts/MainServer/configuration.nix
@@ -175,7 +175,6 @@ in
       root = "/var/www/WatchesWithMark/WatchesWithMark-frontend/dist";
 
       extraConfig = ''
-        # client_max_body_size 20M;
         gzip on;
         gzip_static on;
         gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript image/svg+xml;
@@ -240,8 +239,7 @@ in
       # Restrict access to Strapi admin panel
       locations."~ /(admin|i18n|content-manager|content-type-builder|upload|users-permissions)" = {
         extraConfig = ''
-          # Disable modsecurity for these endpoints
-          ModSecurityEnabled off;
+          client_max_body_size 200M;
 
           allow 84.39.117.57;
           allow 84.39.117.56;
