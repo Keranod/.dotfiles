3a087d413a8469fddc70b7d78b4e19fcb666ceb2
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-24 12:06:21 +0100

    NetworkBox creation

---
+++ hosts/NetworkBox/configuration.nix
@@ -0,0 +1,116 @@
+{ pkgs, lib, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Default settings for EFI
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  fileSystems."/boot" = {
+    fsType = "vfat";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Networking
+  networking.hostName = "NetworkBox";
+  networking.networkmanager.enable = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  programs.dconf.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  # hardware.pulseaudio.enable = false;
+  # security.rtkit.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  # };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile.
+  # To search, go https://search.nixos.org/packages?channel=24.11&
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    nodePackages_latest.nodejs
+    home-manager
+    htop
+  ];
+
+  networking.firewall = {
+    enable = true;
+    # allowedTCPPorts = [
+    #   80
+    #   443
+    # ];
+    # # allowedUDPPorts = [ ];  # No allowed UDP ports
+    # # rejectPackets = true;
+    # # Allow local connections to 5432 but block external
+    # extraCommands = ''
+    #   iptables -A INPUT -p tcp --dport 5432 -s 127.0.0.1 -j ACCEPT
+    #   iptables -A INPUT -p tcp --dport 5432 -j DROP
+    # '';
+  };
+
+  # https://mynixos.com/
+  system.stateVersion = "24.11";
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Enable the OpenSSH service
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false; # Disable password login
+      PermitRootLogin = "no"; # Root login disabled
+      PubkeyAuthentication = true; # Ensure pubkey authentication is enabled
+    };
+  };
+
+  # AdGuardHome
+  services.adguardhome.enable = true;
+}
