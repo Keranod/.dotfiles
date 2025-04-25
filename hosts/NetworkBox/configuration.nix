7b3b8fe1b9cf8243f3a145a550fdddd5295cdad3
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-25 11:51:02 +0100

    testing box

--- hosts/NetworkBox/configuration.nix
+++ hosts/NetworkBox/configuration.nix
@@ -14,8 +14,29 @@
   };
 
   # Networking
-  networking.hostName = "NetworkBox";
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName             = "NetworkBox";
+    networkmanager.enable = false;
+
+    # Static IP on enp3s0
+    interfaces.enp3s0 = {
+      useDHCP       = false;
+      ipv4.addresses = [
+        { address = "192.168.1.2"; prefixLength = 24; }
+      ];
+    };
+
+    defaultGateway = "192.168.1.1";
+    nameservers    = [ "127.0.0.1" ];
+
+    # Enable forwarding if you ever proxy/NAT through this box
+    forwardEnable = true;
+
+    firewall = {
+      enable         = true;
+      allowedUDPPorts = [ 53 ];   # DNS
+    };
+  };
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -75,17 +96,6 @@
     htop
   ];
 
-  networking.firewall = {
-    enable = true;
-    # allowedTCPPorts = [
-    #   80
-    #   443
-    # ];
-    # allowedUDPPorts = [ ];  # No allowed UDP ports
-    # rejectPackets = true;
-    # Allow local connections to 5432 but block external
-  };
-
   # https://mynixos.com/
   system.stateVersion = "24.11";
 
@@ -104,19 +114,49 @@
     };
   };
 
-  # AdGuardHome
+  # AdGuard Home: DNS + DHCP
   services.adguardhome = {
-    enable       = true;
-    openFirewall = true;   # automatically adds port 3000 to allowedTCPPorts
+    enable         = true;
+    openFirewall   = true;   # auto-opens 53 & 3000
     restartOnChange = true;
-    immutable = true;
-    mutableSettings = true;
+    immutable      = true;   # lock UI edits
+    mutableSettings = true;  # re-seed on service start
 
     settings = {
+      # DNS
+      dns = {
+        bind_hosts   = [ "0.0.0.0" ];
+        port         = 53;
+        upstream_dns = [
+          "94.140.14.14"
+          "94.140.15.15"
+        ];
+      };
+
+      # DHCP
+      dhcp = {
+        enabled     = true;
+        interface   = "enp3s0";
+        gateway_ip  = "192.168.1.1";
+        subnet_mask = "255.255.255.0";
+        range_start = "192.168.1.100";
+        range_end   = "192.168.1.200";
+        lease_time  = 86400;
+        options = [
+          # Option 6 = DNS server; we explicitly list AGH AND a fallback
+          "6 ip 192.168.1.2,1.1.1.1"
+        ];
+
+        static_leases = {
+          "AA:BB:CC:DD:EE:FF" = "192.168.1.50";  # TV’s MAC → .50
+        };
+      };
+
+      # Blocklists / filtering (defaults)
       filtering = {
         protection_enabled = true;
-        filtering_enabled = true;
-        parental = false;
+        filtering_enabled  = true;
+        parental           = false;
       };
     };
   };
