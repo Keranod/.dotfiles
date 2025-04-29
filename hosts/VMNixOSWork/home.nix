23480894e28b8f46fbd86c1437badf7afa20b61f
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-29 15:05:48 +0100

    configuring vm

--- hosts/VMNixOSWork/home.nix
+++ hosts/VMNixOSWork/home.nix
@@ -8,22 +8,11 @@ in
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
   # List packages installed in user profile.
   # To search, go https://search.nixos.org/packages?channel=24.11&
   home.packages = with pkgs; [
     vlc
   ];
-
-  nix.nixPath = [ "nixpkgs=${pkgs.path}" ];
 
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
@@ -84,7 +73,4 @@ in
       }
     '';
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
