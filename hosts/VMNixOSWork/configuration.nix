ac25d97c9b1470cf876325286af3af4bdc3d8a9c
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-29 12:25:58 +0100

    configuring vm

--- hosts/VMNixOSWork/configuration.nix
+++ hosts/VMNixOSWork/configuration.nix
@@ -43,87 +43,63 @@
       useDHCP        = false;
       ipv4.addresses = [ { address = "192.168.56.10"; prefixLength = 24; } ];
     };
+  };
 
-    # enable nftables and feed it our rules
-    nftables = {
-      enable  = true;
-      ruleset = ''
-        table ip nat {
-          chain prerouting {
-            type nat hook prerouting priority 0; policy accept;
-            # LAN→Proxy
-            iifname "enp0s8" tcp dport 80 dnat to 192.9.253.50:80
-          }
-          chain output {
-            type nat hook output priority 0; policy accept;
-            # Local router→Proxy
-            oifname "enp0s3" tcp dport 80 dnat to 192.9.253.50:80
-          }
-          chain postrouting {
-            type nat hook postrouting priority 100; policy accept;
-            # Masquerade all WAN egress
-            oifname "enp0s3" masquerade
-          }
-        }
-      '';
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      interface = "enp0s8";
+      bind-interfaces = true;
+
+      # Only DHCP
+      port = 0; # <--- this disables the DNS server in dnsmasq!
+
+      dhcp-range    = "192.168.56.100,192.168.56.200,24h";
+      dhcp-option   = [ "3,192.168.56.10" "6,192.168.56.10" ];
+      dhcp-host = [
+        "7C:F1:7E:6C:60:00,192.168.9.2" # TP-Link
+        "A8:23:FE:FD:19:ED,192.168.9.50" # TV
+      ];
     };
   };
 
-  # services.dnsmasq = {
-  #   enable = true;
-  #   settings = {
-  #     interface = "enp0s8";
-  #     bind-interfaces = true;
+  # AdGuard Home: DNS
+  services.adguardhome = {
+    enable = true;
+    openFirewall = true; # auto-opens 53 & 3000
+    mutableSettings = false; # re-seed on service start
 
-  #     # Only DHCP
-  #     port = 0; # <--- this disables the DNS server in dnsmasq!
+    settings = {
+      # DNS
+      dns = {
+        bind_hosts = [
+          "192.168.56.10"
+        ];
+        port = 53;
+        upstream_dns = [
+          "94.140.14.14"
+          "94.140.15.15"
+        ];
+        # Bootstrap DNS: used only to resolve the upstream hostnames
+        bootstrap_dns = [
+          "9.9.9.10"
+          "149.112.112.10"
+        ];
+      };
 
-  #     dhcp-range    = "192.168.56.100,192.168.56.200,24h";
-  #     dhcp-option   = [ "3,192.168.56.10" "6,192.168.56.10" ];
-  #     dhcp-host = [
-  #       "7C:F1:7E:6C:60:00,192.168.9.2" # TP-Link
-  #       "A8:23:FE:FD:19:ED,192.168.9.50" # TV
-  #     ];
-  #   };
-  # };
+      # DHCP
+      dhcp = {
+        enabled = false;
+      };
 
-  # # AdGuard Home: DNS
-  # services.adguardhome = {
-  #   enable = true;
-  #   openFirewall = true; # auto-opens 53 & 3000
-  #   mutableSettings = false; # re-seed on service start
-
-  #   settings = {
-  #     # DNS
-  #     dns = {
-  #       bind_hosts = [
-  #         "192.168.56.10"
-  #       ];
-  #       port = 53;
-  #       upstream_dns = [
-  #         "94.140.14.14"
-  #         "94.140.15.15"
-  #       ];
-  #       # Bootstrap DNS: used only to resolve the upstream hostnames
-  #       bootstrap_dns = [
-  #         "9.9.9.10"
-  #         "149.112.112.10"
-  #       ];
-  #     };
-
-  #     # DHCP
-  #     dhcp = {
-  #       enabled = false;
-  #     };
-
-  #     # Blocklists / filtering (defaults)
-  #     filtering = {
-  #       protection_enabled = true;
-  #       filtering_enabled = true;
-  #       parental = false;
-  #     };
-  #   };
-  # };
+      # Blocklists / filtering (defaults)
+      filtering = {
+        protection_enabled = true;
+        filtering_enabled = true;
+        parental = false;
+      };
+    };
+  };
 
   # List packages installed in system profile.
   # To search, go https://search.nixos.org/packages?channel=24.11&
