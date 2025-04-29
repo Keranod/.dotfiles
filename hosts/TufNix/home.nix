58956c543ae473f9c88a1af515412181b38a0d4d
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-29 16:26:29 +0100

    cleaned home for laptop

--- hosts/TufNix/home.nix
+++ hosts/TufNix/home.nix
@@ -1,52 +1,16 @@
 { pkgs, ... }:
 
 let
-  username = "keranod";
   pgadminVersion = "4";
   pgadminPackage = pkgs."pgadmin${pgadminVersion}-desktopmode";
 in
 {
-  # Infomration for home-manager which path to manage
-  home.username = username;
-  home.homeDirectory = "/home/${username}";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
-
-  # List packages installed in user profile.
-  # To search, go https://search.nixos.org/packages?channel=24.11&
   home.packages = with pkgs; [
     pgadminPackage
     google-chrome
     vlc
     prismlauncher
   ];
-
-  nix.nixPath = [ "nixpkgs=${pkgs.path}" ];
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/keranod/etc/profile.d/hm-session-vars.sh
-  #
-
   # User services
   systemd.user.services.pgadmin4 = {
     Unit = {
@@ -67,7 +31,4 @@ in
       WantedBy = [ "default.target" ];
     };
   };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
