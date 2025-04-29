834c4eea5d6b65de2882c2f2560e4290cd5c7da5
Konrad Konkel <konrad.konkel@wp.pl>
2025-04-29 11:21:24 +0100

    configuring vm

--- hosts/VMNixOSWork/configuration.nix
+++ hosts/VMNixOSWork/configuration.nix
@@ -57,65 +57,67 @@
         export http_proxy="http://192.9.253.10:80"
         export https_proxy="$http_proxy"
         export no_proxy="127.0.0.1,localhost,192.168.56.10"
+        sudo systemctl set-environment http_proxy="http://proxy:3128" https_proxy="http://proxy:3128"
+        sudo systemctl restart nix-daemon
       fi
     '';
     environment.etc."profile.d/proxy-enp0s3.sh".mode = "0755";
 
-  services.dnsmasq = {
-    enable = true;
-    settings = {
-      interface = "enp0s8";
-      bind-interfaces = true;
+  # services.dnsmasq = {
+  #   enable = true;
+  #   settings = {
+  #     interface = "enp0s8";
+  #     bind-interfaces = true;
 
-      # Only DHCP
-      port = 0; # <--- this disables the DNS server in dnsmasq!
+  #     # Only DHCP
+  #     port = 0; # <--- this disables the DNS server in dnsmasq!
 
-      dhcp-range    = "192.168.56.100,192.168.56.200,24h";
-      dhcp-option   = [ "3,192.168.56.10" "6,192.168.56.10" ];
-      dhcp-host = [
-        "7C:F1:7E:6C:60:00,192.168.9.2" # TP-Link
-        "A8:23:FE:FD:19:ED,192.168.9.50" # TV
-      ];
-    };
-  };
+  #     dhcp-range    = "192.168.56.100,192.168.56.200,24h";
+  #     dhcp-option   = [ "3,192.168.56.10" "6,192.168.56.10" ];
+  #     dhcp-host = [
+  #       "7C:F1:7E:6C:60:00,192.168.9.2" # TP-Link
+  #       "A8:23:FE:FD:19:ED,192.168.9.50" # TV
+  #     ];
+  #   };
+  # };
 
-  # AdGuard Home: DNS
-  services.adguardhome = {
-    enable = true;
-    openFirewall = true; # auto-opens 53 & 3000
-    mutableSettings = false; # re-seed on service start
+  # # AdGuard Home: DNS
+  # services.adguardhome = {
+  #   enable = true;
+  #   openFirewall = true; # auto-opens 53 & 3000
+  #   mutableSettings = false; # re-seed on service start
 
-    settings = {
-      # DNS
-      dns = {
-        bind_hosts = [
-          "192.168.56.10"
-        ];
-        port = 53;
-        upstream_dns = [
-          "94.140.14.14"
-          "94.140.15.15"
-        ];
-        # Bootstrap DNS: used only to resolve the upstream hostnames
-        bootstrap_dns = [
-          "9.9.9.10"
-          "149.112.112.10"
-        ];
-      };
+  #   settings = {
+  #     # DNS
+  #     dns = {
+  #       bind_hosts = [
+  #         "192.168.56.10"
+  #       ];
+  #       port = 53;
+  #       upstream_dns = [
+  #         "94.140.14.14"
+  #         "94.140.15.15"
+  #       ];
+  #       # Bootstrap DNS: used only to resolve the upstream hostnames
+  #       bootstrap_dns = [
+  #         "9.9.9.10"
+  #         "149.112.112.10"
+  #       ];
+  #     };
 
-      # DHCP
-      dhcp = {
-        enabled = false;
-      };
+  #     # DHCP
+  #     dhcp = {
+  #       enabled = false;
+  #     };
 
-      # Blocklists / filtering (defaults)
-      filtering = {
-        protection_enabled = true;
-        filtering_enabled = true;
-        parental = false;
-      };
-    };
-  };
+  #     # Blocklists / filtering (defaults)
+  #     filtering = {
+  #       protection_enabled = true;
+  #       filtering_enabled = true;
+  #       parental = false;
+  #     };
+  #   };
+  # };
 
   # List packages installed in system profile.
   # To search, go https://search.nixos.org/packages?channel=24.11&
