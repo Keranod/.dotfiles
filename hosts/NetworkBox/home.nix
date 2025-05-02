a08c3809d46644102a8458f932f907e085d48300
keranod <konrad.konkel@wp.pl>
2025-05-03 00:11:20 +0100

    vpn

--- hosts/NetworkBox/home.nix
+++ hosts/NetworkBox/home.nix
@@ -2,8 +2,8 @@
   programs.bash = {
     enable = true;
     shellAliases = {
-      add-tv-to-vpn = "sudo ip rule add from 192.168.9.50 table vpn && sudo ip route add default dev wg0 table vpn";
-      remove-tv-from-vpn = "sudo ip rule del from 192.168.9.50 table vpn && sudo ip route del default dev wg0 table vpn";
+      add-tv-to-vpn = "sudo ip rule add from 192.168.9.60 table vpn && sudo ip route add default dev wg0 table vpn";
+      remove-tv-from-vpn = "sudo ip rule del from 192.168.9.60 table vpn && sudo ip route del default dev wg0 table vpn";
     };
   };
 }
