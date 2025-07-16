eee4a7d9af7fecdab379b6b61010456e1694230b
Keranod <konrad.konkel@wp.pl>
2025-07-16 13:01:57 +0100

    debugging

--- hosts/ABYSS/configuration.nix
+++ hosts/ABYSS/configuration.nix
@@ -3,7 +3,7 @@
 let
   domain        = "keranod.dev";
   # read the password at eval time from your local secrets dir
-  secretFile     =  ./.secrets;
+  secretFile     =  ./secrets/trojango.pass;
   trojanPassword = builtins.readFile secretFile;
 in
 {
