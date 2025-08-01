0c9a376804f93e4929c60d66d792665694c52e23
Keranod <konrad.konkel@wp.pl>
2025-08-01 13:26:59 +0100

    installing vaultwarden

--- hosts/ABYSS/configuration.nix
+++ hosts/ABYSS/configuration.nix
@@ -191,6 +191,7 @@ in
 
   services.vaultwarden = {
     enable = true;
+    webVault.enable = true;
     config = {
       rocketPort = 8222; # or whatever port you want
       rocketAddress = "10.100.0.1"; # ← set this to your VPN interface IP
