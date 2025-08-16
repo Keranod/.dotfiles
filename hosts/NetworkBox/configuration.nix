d1130bc38003aa04977189cdd20dd9f07688d86a
keranod <konrad.konkel@wp.pl>
2025-08-16 21:20:37 +0100

    firewall change

--- hosts/NetworkBox/configuration.nix
+++ hosts/NetworkBox/configuration.nix
@@ -184,7 +184,9 @@ in
 
             # CRITICAL: EXPLICITLY DROP all DNS traffic that tries to leave
             # on the physical WAN interface or the wg-vps tunnel.
-            dport { 53, 853 } oifname { "enp3s0", "wg-vps" } drop;
+            dport 53 oifname { "enp3s0", "wg-vps" } drop;
+            dport 853 oifname { "enp3s0", "wg-vps" } drop;
+
 
             # Allow DNS-over-TLS ONLY via the wg-vps2 interface.
             { udp, tcp } dport 853 oifname "wg-vps2" accept;
