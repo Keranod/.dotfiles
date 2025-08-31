2ce8822e584682470431f6f4edb7992e4fb39090
keranod <konrad.konkel@wp.pl>
2025-08-31 07:49:10 +0100

    no autogen wireguard keys

--- hosts/ABYSS/configuration.nix
+++ hosts/ABYSS/configuration.nix
@@ -36,9 +36,7 @@ in
         "vpn-network" = {
           ips = [ "10.0.0.1/24" ];
           listenPort = 51820;
-          generatePrivateKeyFile = true;
           privateKeyFile = "/etc/wireguard/${serverHostName}.key";
-          postSetup = ''wg pubkey > /etc/wireguard/${serverHostName}.pub'';
           peers = [
             {
               name = "NetworkBox";
