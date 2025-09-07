7370a149341d04ac408400ffe34304af0e170ce4
keranod <konrad.konkel@wp.pl>
2025-09-07 17:21:42 +0100

    ngnix fix

--- hosts/NetworkBox/configuration.nix
+++ hosts/NetworkBox/configuration.nix
@@ -419,7 +419,7 @@ in
     recommendedOptimisation = true;
 
     appendHttpConfig = ''
-      large_client_header_buffers 4 16k;
+      large_client_header_buffers 8 32k;
       proxy_headers_hash_max_size 1024;
       proxy_headers_hash_bucket_size 128;
     '';
