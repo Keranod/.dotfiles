77d07648edcf6b3feacbfca01092b8808b42b7bc
keranod <konrad.konkel@wp.pl>
2025-09-07 17:05:11 +0100

    nginx basic auth

--- hosts/NetworkBox/configuration.nix
+++ hosts/NetworkBox/configuration.nix
@@ -501,7 +501,6 @@ in
         extraConfig = ''
           auth_basic "Restricted Access";
           auth_basic_user_file /run/webdav_secrets/webdav.users;
-          proxy_set_header Authorization $http_authorization;
           proxy_pass_request_body on;
           proxy_pass_request_headers on;
           proxy_buffering off;
