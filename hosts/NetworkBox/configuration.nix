45ecb78de3b6abc2d3dac36e4345bdd9e20d02d6
keranod <konrad.konkel@wp.pl>
2025-09-07 17:13:34 +0100

    vaultwarden port declaration

--- hosts/NetworkBox/configuration.nix
+++ hosts/NetworkBox/configuration.nix
@@ -503,17 +503,23 @@ in
           auth_basic "Restricted Access";
           auth_basic_user_file /run/webdav_secrets/webdav.users;
 
-          # Your existing proxy headers
+          proxy_set_header Authorization $http_authorization;
           proxy_pass_request_body on;
           proxy_pass_request_headers on;
           proxy_buffering off;
           proxy_request_buffering off;
 
-          # New headers from the documentation for robust proxying
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header Host $host;
           proxy_redirect off;
+
+          # This is required for COPY/MOVE commands
+          set $dest $http_destination;
+          if ($http_destination ~ "^https://${webdavDomain}(?<path>(.+))") {
+              set $dest https://127.0.0.1:${toString webdavPort}$path;
+          }
+          proxy_set_header Destination $dest;
         '';
       };
     };
