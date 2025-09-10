953b895d9fb3d90f5daade59cb7fc4b0bed0a29c
Keranod <konrad.konkel@wp.pl>
2025-09-10 14:04:56 +0100

    changes

--- hosts/ABYSS/configuration.nix
+++ hosts/ABYSS/configuration.nix
@@ -144,6 +144,7 @@ in
     acceptTerms = true;
     defaults = {
       email = "konrad.konkel@wp.pl";
+      dnsProvider = "hetzner";
       dnsResolver = "10.0.0.2";
     };
     certs."keranod.dev" = {
